8d756e03ed9b6f9daeb4e780ce2c80457ab68721
iamjibran <35794516+iamjibran@users.noreply.github.com>
2018-04-09 09:00:32 +0800

    Final userpages
    the complete User pages and PHP

---
+++ index.php
@@ -0,0 +1,229 @@
+<?php
+
+<<<<<<< HEAD
+//To Handle Session Variables on This Page
+session_start();
+
+//If user Not logged in then redirect them back to homepage. 
+if(empty($_SESSION['id_user'])) {
+  header("Location: ../index.php");
+  exit();
+}
+
+require_once("../db.php");
+=======
+session_start();
+
+if(isset($_SESSION['id_admin'])) {
+  header("Location: dashboard.php");
+  exit();
+}
+
+>>>>>>> 78374467415fbdbfa4897cb31d69425b25aa4007
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Jinjang</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../css/AdminLTE.min.css">
+<<<<<<< HEAD
+  <link rel="stylesheet" href="../css/_all-skins.min.css">
+  <!-- Custom -->
+  <link rel="stylesheet" href="../css/custom.css">
+=======
+  <!-- iCheck -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/blue.css">
+
+>>>>>>> 78374467415fbdbfa4897cb31d69425b25aa4007
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+<<<<<<< HEAD
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-green sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="index.php" class="logo logo-bg">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>J</b>P</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b><?php echo $app_name;?></span>
+    </a>
+
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li>
+            <a href="../jobs.php">Jobs</a>
+          </li>          
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" style="margin-left: 0px;">
+
+    <section id="candidates" class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="box box-solid">
+              <div class="box-header with-border">
+                <h3 class="box-title">Welcome <b><?php echo $_SESSION['name']; ?></b></h3>
+              </div>
+              <div class="box-body no-padding">
+               <ul class="nav nav-pills nav-stacked">
+                  <li><a href="edit-profile.php"><i class="fa fa-user"></i> Edit Profile</a></li>
+                  <li class="active"><a href="index.php"><i class="fa fa-address-card-o"></i> My Applications</a></li>
+                  <li><a href="../jobs.php"><i class="fa fa-list-ul"></i> Jobs</a></li>
+                  <!--<li><a href="mailbox.php"><i class="fa fa-envelope"></i> Mailbox</a></li>-->
+                  <li><a href="settings.php"><i class="fa fa-gear"></i> Settings</a></li>
+                  <li><a href="../logout.php"><i class="fa fa-arrow-circle-o-right"></i> Logout</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-9 bg-white padding-2">
+            <h2><i>Recent Applications</i></h2>
+            <p>Below you will find job roles you have applied for</p>
+
+            <?php
+             $sql = "SELECT * FROM job_post INNER JOIN apply_job_post ON job_post.id_jobpost=apply_job_post.id_jobpost WHERE apply_job_post.id_user='$_SESSION[id_user]'";
+                  $result = $conn->query($sql);
+
+                  if($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) 
+                    {     
+            ?>
+            <div class="attachment-block clearfix padding-2">
+                <h4 class="attachment-heading"><a href="view-job-post.php?id=<?php echo $row['id_jobpost']; ?>"><?php echo $row['jobtitle']; ?></a></h4>
+                <div class="attachment-text padding-2">
+                  <div class="pull-left"><i class="fa fa-calendar"></i> <?php echo $row['createdat']; ?></div>  
+                  <?php 
+
+                  if($row['status'] == 0) {
+                    echo '<div class="pull-right"><strong class="text-orange">Pending</strong></div>';
+                  } else if ($row['status'] == 1) {
+                    echo '<div class="pull-right"><strong class="text-red">Rejected</strong></div>';
+                  } else if ($row['status'] == 2) {
+                    echo '<div class="pull-right"><strong class="text-green">Accepted</strong></div> ';
+                  }
+                  ?>
+                                
+                </div>
+            </div>
+
+            <?php
+              }
+            }
+            ?>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer" style="margin-left: 0px;">
+    <div class="text-center">
+      <strong>Copyright &copy; 2014-2018 <a href=""><?php echo $app_name;?></a>.</strong> All rights
+    reserved.
+    </div>
+  </footer>
+
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+
+</div>
+<!-- ./wrapper -->
+=======
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../index.php"><b>Jinjang</b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Admin Login</p>
+
+    <form action="checklogin.php" method="post">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" name="username" placeholder="Username">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" name="password" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+      <?php 
+//If User Failed To log in then show error message.
+if(isset($_SESSION['loginError'])) {
+  ?>
+  <div>
+    <p class="text-center">Invalid Email/Password! Try Again!</p>
+  </div>
+<?php
+ unset($_SESSION['loginError']); }
+?>
+
+    </form>
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+>>>>>>> 78374467415fbdbfa4897cb31d69425b25aa4007
+
+<!-- jQuery 3 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../js/adminlte.min.js"></script>
+<<<<<<< HEAD
+=======
+<!-- iCheck -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
+
+>>>>>>> 78374467415fbdbfa4897cb31d69425b25aa4007
+</body>
+</html>
